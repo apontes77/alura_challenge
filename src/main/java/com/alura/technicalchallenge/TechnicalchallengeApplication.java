de1bdcf81911c558303bc88d9b13bb5df3ff5289
Alexandre Pontes de Queiroz <alexandrepontesgyn@gmail.com>
2024-05-08 20:53:18 -0300

    feat: initial configurations

---
+++ src/main/java/com/alura/technicalchallenge/TechnicalchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alura.technicalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalchallengeApplication.class, args);
+	}
+
+}
