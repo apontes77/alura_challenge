59b2b699f3280b3590ea4f635361a2c6cc8901c0
Alexandre Pontes de Queiroz <alexandrepontesgyn@gmail.com>
2024-05-12 21:35:09 -0300

    fix: refactoring regex and use @Email validation

--- src/main/java/com/alura/technicalchallenge/controller/user/request/UserRegisterRequest.java
+++ src/main/java/com/alura/technicalchallenge/controller/user/request/UserRegisterRequest.java
@@ -1,49 +1,27 @@
 package com.alura.technicalchallenge.controller.user.request;
 
 import com.alura.technicalchallenge.domain.enums.Role;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class UserRegisterRequest {
 
     private String name;
 
-    @Pattern(regexp = "^[a-z]*$", message = "username must contain only lowercase characters")
+    @Pattern(regexp = "^[a-z]+$", message = "Username must contain only lowercase characters")
     private String username;
-    @Pattern(regexp = "/^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+\\.([a-z]+)?$/i", message = "e-mail must be valid")
+
+    @Email
     private String email;
-    @Pattern(regexp = "(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,10})$", message = "password must be between 8 and 10 characters, contain at least one\n" +
-            "digit and one alphabetic character, and must not contain special characters")
+
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}", message = "Password must be 8-10 characters, contain at least one digit, one lowercase letter, and one uppercase letter")
     private String password;
+
     private Role role;
-
-    public UserRegisterRequest() {
-    }
-
-    public UserRegisterRequest(String name, String username, String email, String password, Role role) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
 }
