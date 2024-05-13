8855fbe6f6094d2ed0d2688e883f888b37762b99
Alexandre Pontes de Queiroz <alexandrepontesgyn@gmail.com>
2024-05-12 21:34:15 -0300

    fix: refactoring migrations

--- src/main/resources/db/migration/V2__create-table-course.sql
+++ src/main/resources/db/migration/V2__create-table-course.sql
@@ -1,10 +1,10 @@
-CREATE TABLE course (
+CREATE TABLE course_tb (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     code VARCHAR(10) unique,
     instructor VARCHAR(255),
     description TEXT,
-    status ENUM('ACTIVE', 'INACTIVE') NOT NULL,
+    status VARCHAR(20) NOT NULL,
     creation_date DATE,
     inactivation_date DATE
 );
--- src/main/resources/db/migration/V3__create-table-enrollment.sql
+++ src/main/resources/db/migration/V3__create-table-enrollment.sql
@@ -1,4 +1,4 @@
-CREATE TABLE enrollment (
+CREATE TABLE enrollment_tb (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     course_id INT,
